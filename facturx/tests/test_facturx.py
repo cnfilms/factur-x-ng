2e3d448542bfdf65032be61228cfe60ffe08b6bf
duskybomb <harshit113@ducic.ac.in>
2018-05-24 13:40:02 +0530

    Initial test need to add more

--- facturx/tests/test_facturx.py
+++ facturx/tests/test_facturx.py
@@ -1,6 +1,7 @@
 import os
 import unittest
-from facturx.facturx import FacturX
+from facturx.facturx import *
+
 
 class TestReading(unittest.TestCase):
     def setUp(self):
@@ -16,8 +17,25 @@ class TestReading(unittest.TestCase):
         with self.assertRaises(TypeError) as context:
             FacturX('non-existant.pdf')
 
+    def test_file_without_embedded_data(self):
+        for file in self.test_files:
+            if file == 'no_embedded_data.pdf':
+                file_path = os.path.join(self.test_files_dir, file)
+                self.assertEqual(FacturX(file_path)._xml_from_file(file_path), None)
+
+    def test_file_embedded_data(self):
+        for file in self.test_files:
+            if file == 'embedded_data.pdf':
+                file_path = os.path.join(self.test_files_dir, file)
+                if FacturX(file_path)._xml_from_file(file_path) is None:
+                    self.assertTrue(False, "The PDF file has no embedded file")
+                else:
+                    self.assertTrue(True)
+
+
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
